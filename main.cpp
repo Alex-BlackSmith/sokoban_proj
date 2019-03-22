6660d3aa62242f753bd0d14fedf580669e665706
Alexander Kuznetsov <akuznetsov@localhost.localdomain>
2019-03-22 10:19:39 +0300

    Test commint on GitHub

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <libtcod.hpp>
+#include <iostream>
+#include <string>
+using std::string;
+using std::cout;
+using std::endl;
+
+void print(const char *cp){
+    if(cp)
+        while(*cp)
+            cout << *cp++;
+}
+int main() {
+
+    //TCODConsole::setCustomFont("standard_8x8_ascii_in_col_font.bmp",TCOD_FONT_LAYOUT_ASCII_INCOL);
+    char *str = "string";
+
+    //TCODConsole::initRoot(80,60,"AlexSmith's Sokoban", false, TCOD_RENDERER_GLSL);
+    //TCODConsole::root -> TCODConsole::vline(0,0, 40, TCOD_BKGND_ALPHA(1));
+   print(str);
+
+
+    return 0;
+}
+
