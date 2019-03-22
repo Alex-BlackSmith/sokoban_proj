7641b80d096f92bf1dff54ced0103f69e03648d4
root <kuznec3093@gmail.com>
2019-03-22 15:19:32 +0300

    implementation prototype of the readMap function adding comments to function and class

--- main.cpp
+++ main.cpp
@@ -1,20 +1,41 @@
 #include <libtcod.hpp>
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <ostream>
 using std::string;
 using std::cout;
 using std::endl;
+using std::cin;
+using std::fstream;
+using std::getline;
 
+//that function reads txt file and load dimensions of map
+//and store them into len and wth
+// although store strings in map into 1-dim array of chars
+void readMap(const string& filename) {
+    unsigned len, wth;
+    fstream mapFile;
+    mapFile.open(filename);
+    if (mapFile) {
+        mapFile >> len >> wth;
+    }
+    const char *strArr[wth*len];
+    cout << len << " " << wth;
+    return;
+}
+
+//implementation of the template class of 2DArray
 template <typename T>
 class TwoDimArray {
 public:
     TwoDimArray() = default;
-    TwoDimArray(T argValue, unsigned argLen, unsigned argWth) : length(argLen), width(argWth)
+    TwoDimArray(const unsigned argLen,const unsigned argWth) : length(argLen), width(argWth)
     {
         value = new T *[length];
         for (auto row : value)
         {
-            row = new T[argWth];
+            row = new T[width];
         }
     }
 
@@ -26,6 +47,7 @@ public:
     TwoDimArray getArray () {
         return this;
     };
+    ~TwoDimArray() {}
 private:
     unsigned length;
     unsigned width;
@@ -33,8 +55,8 @@ private:
 };
 
 int main() {
-    TCODConsole::initRoot(80,60,"AlexSmith's Sokoban", false, TCOD_RENDERER_GLSL);
-
+    //TCODConsole::initRoot(80,60,"AlexSmith's Sokoban", false, TCOD_RENDERER_GLSL);
+    readMap("TestMap.txt");
 
 
 
